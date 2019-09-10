6980ad689d57219c1ad61a6be1bfc346052754b6
Lucas Moesch <lucasmoesch@live.de>
2019-09-10 20:51:17 +0000

    WIP

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { WavPlayer } from './core/wav-player'
+import { StreamPlayer } from './core/stream-player'
 import { AppService } from './app.service';
 import { WavChannel } from './core/wav-channel';
 
 @Controller()
 export class AppController {
   private _player: WavPlayer = new WavPlayer();
+  private _streamer: StreamPlayer = new StreamPlayer();
 
   private _droneActive: boolean = false;
+  private _droneStart: number = 0;
+  private _droneLoop = null;
+
+  private _streamActive = false;
 
   constructor(private _appService: AppService) {
 
@@ -36,10 +42,21 @@ export class AppController {
     }
   }
 
+  @Post('live') 
+  public async playLive(@Body() body: {status: string}) {
+    if(body.status == 'start' && !this._streamActive) {
+      this._streamActive = true;
+      await this._streamer.play();
+    }
+
+    if(body.status == 'stop' && this._streamActive) {
+      await this._streamer.stop();
+      this._streamActive = false;
+    }
+  }
+
   @Post('drone')
   public async playDrone(@Body() body: {status: string}) {
-    let loop = null;
-    let start = 0;
     const drone_loop_duration = 29902;
     const drone_intro_duration = 21895;
     const drone_outro_duration = 9682;
@@ -47,20 +64,28 @@ export class AppController {
     if (!this._droneActive && body.status == 'start') {
       this._droneActive = true;
       this._player.play({path: './src/wav/drohne-anflug-v1.wav'})
-      setTimeout(() => {
-        start = Date.now()
-        this._player.play({path: './src/wav/Drohne-mitte-loop.wav'})
-      }, drone_intro_duration - 500)
 
-      loop = setInterval(() => {
+      setTimeout(() => {
+        this._droneStart = Date.now()
         this._player.play({path: './src/wav/Drohne-mitte-loop.wav'})
-      }, drone_loop_duration - 500)
+
+        if (this._droneActive){
+          this._droneLoop = setInterval(() => {
+            if (this._droneActive){
+              this._droneStart = Date.now();
+              this._player.play({path: './src/wav/Drohne-mitte-loop.wav'})
+            }
+          }, drone_loop_duration - 410)
+        }
+      }, drone_intro_duration - 410)
+
     } else if(this._droneActive && body.status == 'stop') {
-      clearInterval(loop);
+      clearInterval(this._droneLoop);
+      this._droneActive = false;
       setTimeout(() => {
         this._player.play({path: './src/wav/drohne-abflug.wav'})
         setTimeout(() => {this._droneActive = false;}, )
-      }, drone_loop_duration - (Date.now() - start) - 500);
+      }, drone_loop_duration - (Date.now() - this._droneStart) - 410);
     }
   }
 }
--- src/core/stream-player.ts
+++ src/core/stream-player.ts
@@ -2,10 +2,16 @@ import { ChildProcess, spawn } from 'child_process';
 
 export class StreamPlayer {
     private _proc = null;
-
+    private _webstreamURL = 'http://192.168.178.26:8080'
 
     public async play(){
-        // this._proc = spawn();
+        console.log('play')
+        this._proc = spawn('mpg123', [this._webstreamURL]);
     };
-    public stop(){};
+    public stop(){
+        if (this._proc) {
+            this._proc.kill();
+            this._proc = null;
+        }
+    };
 }
